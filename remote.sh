3d8271ffaa3fb6bb0aee21b3b69266d5955b6137
Stephen J Carnam <steve@steveorevo.com>
2025-02-21 08:41:51 -0800

    wip

--- remote.sh
+++ remote.sh
@@ -21,6 +21,12 @@ update-grub
 echo "Removing AppArmor."
 apt remove -y apparmor
 
+# Add ll globally
+alias ll='ls -alF'
+cat <<EOT >> /etc/bash.bashrc
+alias ll='ls -alF'
+EOT
+
 # Download HesitaCP Installer
 echo "Downloading HestiaCP Installer."
 cd /tmp
@@ -29,11 +35,6 @@ wget https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-ins
 # Install HestiaCP
 echo "Installing HestiaCP. Force appears to be broken, lookout for prompts to answer manually."
 bash hst-install.sh --apache yes --phpfpm yes --multiphp yes --vsftpd yes --proftpd no --named no --mariadb yes --mysql8 no --postgresql yes --exim no --dovecot no --sieve no --clamav no --spamassassin no --iptables yes --fail2ban no --quota no --api yes --interactive no --with-debs no  --port '8083' --hostname 'local.dev.pw' --email 'devstia@dev.pw' --username 'admin' --password 'personalweb' --lang 'en' --webterminal yes
-
-# Add ll globally
-cat <<EOT >> /etc/bash.bashrc
-alias ll='ls -alF'
-EOT
 
 # Fix phpmyadmin permissions
 echo "Fixing phpMyAdmin permissions."
