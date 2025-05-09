d5712229a192bf6d87eba1f17e90caa7dbe97864
Stephen J Carnam <steve@steveorevo.com>
2025-05-08 21:37:44 -0700

    wip

--- build-mac-x64.sh
+++ build-mac-x64.sh
@@ -27,3 +27,21 @@ else
     echo "debian-amd64.zip already exists in the build folder."
 fi
 
+# Create an empty disk for the overlay
+
+# Spawn the VM with the debian-amd64 base image
+qemu-system-x86_64 \
+    -machine q35,vmport=off -accel hvf \
+    -cpu qemu64-v1 \
+    -vga virtio \
+    -smp cpus=4,sockets=1,cores=4,threads=1 \
+    -m 4G \
+    -bios bios.img \
+    -display default,show-cursor=on \
+    -net nic -net user,hostfwd=tcp::8022-:22,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,hostfwd=tcp::8083-:8083 \
+    -drive if=virtio,format=qcow2,file=devstia-amd64.img \
+    -device virtio-balloon-pci \
+    -device virtio-serial-pci \
+    -chardev socket,path=/tmp/qga.sock,server=on,wait=off,id=qga0 \
+    -device virtserialport,chardev=qga0,name=org.qemu.guest_agent.0 \
+    -nographic
